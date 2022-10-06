c52e10cf9dedfb5144a3853f0ad1cce01e3f999c
trayand <patradyn@gmail.com>
2022-10-06 12:33:59 +0700

    add onClick function to ButtonAction

--- src/components/button-action/index.jsx
+++ src/components/button-action/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 
-export default function ButtonAction({ children, injectClass = "" }) {
+export default function ButtonAction({ children, onClick, injectClass = "" }) {
     return (
-        <div>
+        <div ocClick={onClick}>
             <button class={"w-36 my-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded " + injectClass}>
                 {children}
             </button>
